2c3082ab7e0fa7072205e5f890f065c04f97df74
ankeetkarki <ankeetkarki30@gmail.com>
2020-09-12 16:06:08 +0545

    Appbar title

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webapp/component/colors.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,11 +9,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Yo",
-          style: TextStyle(color: Colors.blue),
+        centerTitle: false,
+        elevation: 0,
+        leading: Container(
+          margin: EdgeInsets.only(left: size.width - size.width * 0.9),
+          height: 50,
+          width: 60,
+          color: color1,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Yo",
+              style: TextStyle(color: color3),
+            ),
+          ),
         ),
         backgroundColor: Colors.white,
       ),
