40f8c1f9a49da474c4356e74977351fd32a94615
ankeetkarki <ankeetkarki30@gmail.com>
2020-09-13 10:52:50 +0545

    almost every widgets included except model image

--- lib/component/colors.dart
+++ lib/component/colors.dart
@@ -4,3 +4,5 @@ Color color1 = Color(0xFFE75A58);
 Color color2 = Color(0xFF1F1816);
 Color color3 = Color(0xFFFFFFFF);
 Color color4 = Color(0xFFF0C8A9);
+Color color5 = Color(0xFFF8ECEC);
+Color color6 = Color(0xFFB64B47);
--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    var sizeC = size.width * 0.65;
+    var sizeB = size.width * 0.12;
     return Scaffold(
       appBar: buildAppBar(size),
       body: Container(
@@ -28,13 +30,105 @@ class _HomePageState extends State<HomePage> {
                 color: color4,
               ),
             ),
-            Align(
-              alignment: Alignment.bottomRight,
+            Positioned(
+              top: size.height * 0.6,
+              left: size.width - sizeC - sizeB,
               child: Container(
-                height: size.height * 0.85,
-                width: size.width * 0.65,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [color1, color4])),
+                height: size.height * 0.08,
+                width: sizeB,
+                color: color6,
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "EXPLORE\t",
+                        style: TextStyle(color: color2, fontSize: 22),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 3.0),
+                        child: Icon(
+                          Icons.navigate_next,
+                          color: color2,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 18, 18),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  height: size.height * 0.85,
+                  width: sizeC,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [color1, color4])),
+                ),
+              ),
+            ),
+            // Image.asset('assets/an.jpg')
+
+            Positioned(
+              top: size.height * 0.83,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: size.height * 0.05,
+                      width: size.height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: color4,
+                      ),
+                      child: Center(
+                        child: InkWell(
+                          child: Image.asset('assets/facebook.png'),
+                          onTap: () {},
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.01,
+                    ),
+                    Container(
+                      height: size.height * 0.05,
+                      width: size.height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: color4,
+                      ),
+                      child: Center(
+                        child: InkWell(
+                          child: Image.asset('assets/twitter.png'),
+                          onTap: () {},
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.01,
+                    ),
+                    Container(
+                      height: size.height * 0.05,
+                      width: size.height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: color4,
+                      ),
+                      child: Center(
+                        child: InkWell(
+                          child: Image.asset('assets/instagram-sketched.png'),
+                          onTap: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
